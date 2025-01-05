d20863ea3b187d01dd47dcf19c83ef4f2e31267a
Andrew Kicklighter <apkicklighter@gmail.com>
2025-01-04 23:50:08 -0600

    add schedule endpoint to grab all schedule for team

--- src/main/kotlin/com/fcfb/arceus/service/fcfb/game/ScheduleService.kt
+++ src/main/kotlin/com/fcfb/arceus/service/fcfb/game/ScheduleService.kt
@@ -2,10 +2,12 @@ package com.fcfb.arceus.service.fcfb.game
 
 import com.fcfb.arceus.domain.Schedule
 import com.fcfb.arceus.repositories.ScheduleRepository
+import com.fcfb.arceus.service.fcfb.SeasonService
 import org.springframework.stereotype.Component
 
 @Component
 class ScheduleService(
+    private val seasonService: SeasonService,
     private val scheduleRepository: ScheduleRepository,
 ) {
     /**
@@ -23,4 +25,25 @@ class ScheduleService(
         gameToSchedule.started = true
         scheduleRepository.save(gameToSchedule)
     }
+
+    /**
+     * Get an opponent team
+     * @param team
+     */
+    fun getTeamOpponent(team: String) =
+        scheduleRepository.getTeamOpponent(
+            seasonService.getCurrentSeason().seasonNumber,
+            seasonService.getCurrentWeek(),
+            team,
+        )
+
+    /**
+     * Get the schedule for a given season for a team
+     * @param season
+     * @param team
+     */
+    fun getScheduleBySeasonAndTeam(
+        season: Int,
+        team: String,
+    ) = scheduleRepository.getScheduleBySeasonAndTeam(season, team)
 }
