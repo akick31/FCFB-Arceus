394f1cabc49313a4265c89058beb36fe8e149485
Andrew Kicklighter <apkicklighter@gmail.com>
2025-01-10 00:21:40 -0600

    update cors origin

--- src/main/kotlin/com/fcfb/arceus/filters/CorsFilter.kt
+++ src/main/kotlin/com/fcfb/arceus/filters/CorsFilter.kt
@@ -13,18 +13,49 @@ class CorsFilter : OncePerRequestFilter() {
         response: HttpServletResponse,
         filterChain: FilterChain,
     ) {
-        response.addHeader("Access-Control-Allow-Origin", "*")
-        response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD")
-        response.addHeader(
-            "Access-Control-Allow-Headers",
-            "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
-        )
-        response.addHeader(
-            "Access-Control-Expose-Headers",
-            "Access-Control-Allow-Origin, Access-Control-Allow-Credentials",
-        )
-        response.addHeader("Access-Control-Allow-Credentials", "false")
-        response.addIntHeader("Access-Control-Max-Age", 10)
+        // Handle preflight request (OPTIONS)
+        if ("OPTIONS" == request.method) {
+            response.status = HttpServletResponse.SC_OK
+
+            val allowedOrigins = setOf("https://fakecollegefootball.com")
+            val origin = request.getHeader("Origin")
+
+            if (origin != null && origin in allowedOrigins) {
+                response.addHeader("Access-Control-Allow-Origin", origin)
+                response.addHeader("Access-Control-Allow-Credentials", "true")
+            }
+
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD")
+            response.addHeader(
+                "Access-Control-Allow-Headers",
+                "Origin, " +
+                        "Accept, " +
+                        "X-Requested-With, " +
+                        "Content-Type, " +
+                        "Authorization, " +
+                        "Access-Control-Request-Method, " +
+                        "Access-Control-Request-Headers",
+            )
+            response.addIntHeader("Access-Control-Max-Age", 3600) // Cache preflight request for 1 hour
+        } else {
+            // Allow cross-origin requests from any origin
+            response.addHeader("Access-Control-Allow-Origin", "*")
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD")
+            response.addHeader(
+                "Access-Control-Allow-Headers",
+                "Origin, " +
+                        "Accept, " +
+                        "X-Requested-With, " +
+                        "Content-Type, " +
+                        "Authorization, " +
+                        "Access-Control-Request-Method, " +
+                        "Access-Control-Request-Headers",
+            )
+            response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials")
+            response.addHeader("Access-Control-Allow-Credentials", "false")
+        }
+
+        // Continue with the filter chain
         filterChain.doFilter(request, response)
     }
 }
