0a234e3f3e32646b768c489b167bb4afa796c671
Andrew Kicklighter <apkicklighter@gmail.com>
2025-01-14 01:29:39 -0600

    remove auto proxy

--- src/main/kotlin/com/fcfb/arceus/FcfbArceusApplication.kt
+++ src/main/kotlin/com/fcfb/arceus/FcfbArceusApplication.kt
@@ -2,12 +2,10 @@ package com.fcfb.arceus
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.EnableAspectJAutoProxy
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
-@EnableAspectJAutoProxy
 open class FcfbArceusApplication
 
 fun main(args: Array<String>) {
